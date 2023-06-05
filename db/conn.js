ae6983109d0ab5c9768b5125b66848c071a75d30
NomanGhayyur <nomanghayyur96@gmail.com>
2023-06-05 22:54:07 +0500

    added a connection with database

--- db/conn.js
+++ db/conn.js
@@ -1,3 +1,12 @@
 const mongoose = require('mongoose');
 
-const db = ""
+const db = process.env.DATABASE
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connection Successful')
+}).catch((e) => {
+    console.log(e)
+})
